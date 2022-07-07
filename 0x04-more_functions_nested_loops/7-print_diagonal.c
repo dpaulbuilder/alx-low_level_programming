b676d9c5eed979348abc6936cad75930c1fd69e0
dpaulbuilder <alabipaulolawumi@gmail.comcom>
2022-07-07 09:45:16 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,7 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - a function that draws a diagonal line on the terminal
+ * @n: input numbers of time '\' should be printed
+ * Return: a diagonal
+ */
