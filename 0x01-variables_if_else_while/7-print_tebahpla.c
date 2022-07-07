1364dcce4cda9686c6dd8237f7b1bff3abb92806
dpaulbuilder <alabipaulolawumi@gmail.comcom>
2022-07-06 18:31:23 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 122; a > 96; a--)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
