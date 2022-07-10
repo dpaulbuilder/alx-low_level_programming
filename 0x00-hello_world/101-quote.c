dc665208b46c87532d948ac06c4762fd29cb42f8
dpaulbuilder <alabipaulolawumi@gmail.comcom>
2022-07-10 14:13:44 -0700

    0x00-hello_world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 1 (Error)
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\"- Dora korpar, 2015-10-19\n", stderr);
+	return (1);
+}
