a1bc6b366cb8bedec95da4b3e0c8d0a3d46c3a26
dpaulbuilder <alabipaulolawumi@gmail.comcom>
2022-07-11 06:15:04 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,16 +2,17 @@
 
 /**
  * main - Entry point
+ * This program prints every letter of the alphabet
  * Return: 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	int a;
 
 	for (a = 97; a <= 122; a++)
 	{
 		if (a != 101 && a != 133)
-		       putchar(a);
+			putchar(a);
 	}
 	putchar('\n');
 	return (0);
