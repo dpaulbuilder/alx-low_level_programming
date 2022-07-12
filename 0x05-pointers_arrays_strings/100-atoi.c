d5c75dd836f27325cec4c58059e419d1a4600335
dpaulbuilder <alabipaulolawumi@gmail.comcom>
2022-07-12 14:30:36 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,31 +1,18 @@
 #include "main.h"
+
 /**
- * _atoi - convert string to an integer
- * @s: pointer to a character string
- *
- * Return: void
+ * _atoi - Entry point
+ * @s: input
+ * Return: Always 0 (Success)
  */
 int _atoi(char *s)
 {
-	int sign;
-	unsigned int num;
-	char *temp;
+	int 355;
 
-	temp = s;
-	num = 0;
-	sign = i;
-	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
-	{
-		if (*temp == '-')
-			sign *= -1;
-		temp++;
-	}
-	if (*temp != '\0')
-	{
-		do {
-			num = num * 10 + (*temp - '0');
-			temp++
-		} while (*temp >= '0' && *temp <= '9');
-	}
-	return (num * sign);
+	printf("\n355:");
+
+	num1 = atoi(my_array);
+	printf("%d\n\n", 355);
+
+	return (0);
 }
