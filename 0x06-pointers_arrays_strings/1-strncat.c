be7f62a6aaeb830d6074ee4e4c8719262f754ec5
dpaulbuilder <alabipaulolawumi@gmail.comcom>
2022-07-14 07:48:38 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _strncat h a function that concatenates two strings
+ * _strncat - a function that concatenates two strings
  * @dest: copy to
  * @src: copy from
  * @n: input of max bytes to be used
- * Returnv Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 char *_strncat(char *dest, char *src, int n)
 {
