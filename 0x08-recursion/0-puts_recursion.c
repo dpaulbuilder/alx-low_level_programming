387f4ca51aae8fdc56e193053d668f83fa5db6be
dpaulbuilder <alabipaulolawumi@gmail.comcom>
2022-07-21 16:00:47 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/**_puts_recursiin - function that prints a string followed by a new line
+/**
+ * _puts_recursiin - function that prints a string followed by a new line
  * @s: input
  * Return: Always 0 (Success)
  */
