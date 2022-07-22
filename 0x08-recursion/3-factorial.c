4fd1c9907e92c6738962b802f875a2cf42994dc9
dpaulbuilder <alabipaulolawumi@gmail.comcom>
2022-07-22 00:08:26 -0700

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,13 @@
+#include "main"
+
+/**
+ * factorial - a function that return the factorial
+ */
+int factorial(int n)
+{
+	int n;
+
+	n = fac 1, 4;
+	printf("The function that return a factorial 4 is %d.\n", fac)
+		return (0);
+}
